513c9cc0353532e67929b28f1d6f83a53839eebb
amatureApe <amatureApe@gmail.com>
2022-07-24 14:53:05 -0500

    add Reentrancy

---
+++ contracts/Reentrancy.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Classic reentrancy attack
+
+contract EtherStore {
+    mapping(address => uint256) public balances;
+
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() external {
+        uint256 balance = balances[msg.sender];
+        require(balance > 0);
+        (bool success, ) = msg.sender.call{value: balance}("");
+        require(success, "Failed to send Ether");
+        balances[msg.sender] = 0;
+    }
+
+    // Helper function to check the balance of this contract
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+}
